1fe9b3bcc644f830ce5741daf9aa940f3830259c
MumuNiMochii <deanharoldpabad@gmail.com>
2021-12-09 23:31:20 +0800

    test

--- src/webpage/home/script.js
+++ src/webpage/home/script.js
@@ -1,5 +1,13 @@
 function follow(mouse_position) {
     var ghost = document.getElementById("animated_ghost_emoji_container");
+    var iframe_position = document.getElementsByClassName("responsive_content_iframe").position();
+    
+    iframe_position.innerHTML.onmousemove = function(e) {
+        ghost.style.left = mouse_position.pageX + iframe_position.left + 'px';
+        ghost.style.top = mouse_position.pageY + iframe_position.top + 'px';
+        ghost.style.transition = "all 2s linear 0s";
+    };
+
     ghost.style.left = mouse_position.pageX + 'px';
     ghost.style.top = mouse_position.pageY + 'px';
     ghost.style.transition = "all 2s linear 0s";
